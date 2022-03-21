6f05fc38c1a52dc424ce4053db7b28e1f12ae61f
LeadDev <leaddev@anon.farm>
2022-03-21 15:51:57 +0530

    style: fix formatting

--- test/Yield.spec.ts
+++ test/Yield.spec.ts
@@ -15,28 +15,22 @@ import {
   IERC20,
   IVBase,
   IVToken,
-
   IMiniChefV2,
   IUniswapV2Factory,
   IUniswapV2Pair,
   IUniswapV2Router02,
   IUniswapV3Pool,
-
   IAggregatorV3Interface,
-
   IOracle,
   IVPoolWrapper,
   ClearingHouse,
-
   BaseSushiVault,
-  VaultTest
+  VaultTest,
 } from '../typechain-types';
 
 import { SushiParamsStruct } from '../typechain-types/VaultTest';
 
-import {
-  tickToSqrtPriceX96,
-} from './utils/price-tick';
+import { tickToSqrtPriceX96 } from './utils/price-tick';
 
 import { stealFunds, tokenAmount } from './utils/stealFunds';
 
@@ -48,11 +42,10 @@ const createUsers = async () => {
   let bob = signers[2];
   let carol = signers[3];
 
-  return { admin, alice, bob, carol }
-}
+  return { admin, alice, bob, carol };
+};
 
 const deployAndSetupCore = async () => {
-
   const realToken = await hre.ethers.getContractAt('IWETH', arbConstants.WETH_ADDRESS);
   const realBase = await hre.ethers.getContractAt('IERC20', arbConstants.USDC_ADDRESS);
 
@@ -94,25 +87,30 @@ const deployAndSetupCore = async () => {
 
   const vBase: IVBase = await hre.ethers.getContractAt('IVBase', await rageTradeFactory.vBase());
 
-  const clearingHouse: ClearingHouse = await hre.ethers.getContractAt('ClearingHouse', await rageTradeFactory.clearingHouse());
+  const clearingHouse: ClearingHouse = await hre.ethers.getContractAt(
+    'ClearingHouse',
+    await rageTradeFactory.clearingHouse(),
+  );
 
-  await (await rageTradeFactory.initializePool({
-    deployVTokenParams: {
-      vTokenName: 'vWETH',
-      vTokenSymbol: 'vWETH',
-      rTokenDecimals: 18,
-    },
-    rageTradePoolInitialSettings: {
-      initialMarginRatio: 20_000,
-      maintainanceMarginRatio: 10_000,
-      twapDuration: 300,
-      whitelisted: false,
-      oracle: oracle.address,
-    },
-    liquidityFeePips: 1000,
-    protocolFeePips: 500,
-    slotsToInitialize: 100,
-  })).wait();
+  await (
+    await rageTradeFactory.initializePool({
+      deployVTokenParams: {
+        vTokenName: 'vWETH',
+        vTokenSymbol: 'vWETH',
+        rTokenDecimals: 18,
+      },
+      rageTradePoolInitialSettings: {
+        initialMarginRatio: 20_000,
+        maintainanceMarginRatio: 10_000,
+        twapDuration: 300,
+        whitelisted: false,
+        oracle: oracle.address,
+      },
+      liquidityFeePips: 1000,
+      protocolFeePips: 500,
+      slotsToInitialize: 100,
+    })
+  ).wait();
 
   const eventFilter = rageTradeFactory.filters.PoolInitlized();
   const events = await rageTradeFactory.queryFilter(eventFilter, 'latest');
@@ -122,11 +120,17 @@ const deployAndSetupCore = async () => {
   const vPoolWrapper: IVPoolWrapper = await hre.ethers.getContractAt('IVPoolWrapper', events[0].args[2]);
 
   return { vBase, vToken, realBase, realToken, clearingHouse, oracle, vPool, vPoolWrapper };
-}
+};
 
 const deployAndSetupHelpers = async () => {
-  const uniV2Factory: IUniswapV2Factory = await hre.ethers.getContractAt('IUniswapV2Factory', arbConstants.SUSHI_FACTORY_ADDRESS);
-  const uniV2Router: IUniswapV2Router02 = await hre.ethers.getContractAt('IUniswapV2Router02', arbConstants.SUSHI_ROUTER_ADDRESS);
+  const uniV2Factory: IUniswapV2Factory = await hre.ethers.getContractAt(
+    'IUniswapV2Factory',
+    arbConstants.SUSHI_FACTORY_ADDRESS,
+  );
+  const uniV2Router: IUniswapV2Router02 = await hre.ethers.getContractAt(
+    'IUniswapV2Router02',
+    arbConstants.SUSHI_ROUTER_ADDRESS,
+  );
   const sushiChef: IMiniChefV2 = await hre.ethers.getContractAt('IMiniChefV2', arbConstants.SUSHI_CHEF_ADDRESS);
 
   const weth: IWETH = await hre.ethers.getContractAt('IWETH', arbConstants.WETH_ADDRESS);
@@ -134,45 +138,43 @@ const deployAndSetupHelpers = async () => {
   const wethUsdc: IUniswapV2Pair = await hre.ethers.getContractAt('IUniswapV2Pair', arbConstants.WETH_USDC_PAIR);
   const sushiToken: IERC20 = await hre.ethers.getContractAt('IERC20', arbConstants.SUSHI_ADDRESS);
 
-  return { uniV2Factory, uniV2Router, sushiChef, weth, usdc, wethUsdc, sushiToken }
-}
+  return { uniV2Factory, uniV2Router, sushiChef, weth, usdc, wethUsdc, sushiToken };
+};
 
 const deployAndSetupVault = async (vWETH: string) => {
   const baseSushiVaultFactory = await hre.ethers.getContractFactory('BaseSushiVault');
   const baseSushiVault: BaseSushiVault = await baseSushiVaultFactory.deploy(
     arbConstants.WETH_USDC_PAIR, // _asset
-    arbConstants.VAULT_NAME,     // _name
-    arbConstants.VAULT_SYMBOL,   // _symbol
-    vWETH                        // _vWethAddress
-  )
+    arbConstants.VAULT_NAME, // _name
+    arbConstants.VAULT_SYMBOL, // _symbol
+    vWETH, // _vWethAddress
+  );
   await baseSushiVault.deployed();
 
   const vaultTestFactory = await hre.ethers.getContractFactory('VaultTest');
   const vaultTest = await vaultTestFactory.deploy(
     arbConstants.WETH_USDC_PAIR, // _asset
-    arbConstants.VAULT_NAME,     // _name
-    arbConstants.VAULT_SYMBOL,   // _symbol
-    vWETH                        // _vWethAddress
-  )
+    arbConstants.VAULT_NAME, // _name
+    arbConstants.VAULT_SYMBOL, // _symbol
+    vWETH, // _vWethAddress
+  );
 
   return { baseSushiVault, vaultTest };
-}
+};
 
 const deployAndSetupChainlinkOralces = async () => {
-
   let usdcOracle = await smock.fake<IAggregatorV3Interface>('IAggregatorV3Interface');
   let wethOracle = await smock.fake<IAggregatorV3Interface>('IAggregatorV3Interface');
 
-  return { usdcOracle, wethOracle }
-}
+  return { usdcOracle, wethOracle };
+};
 
 const mineBlocks = async (blocks: number) => {
   for (let i = 0; i < blocks; i++) {
-    await network.provider.send("evm_increaseTime", [3600])
-    await network.provider.send("evm_mine") // this one will have 02:00 PM as its timestamp
+    await network.provider.send('evm_increaseTime', [3600]);
+    await network.provider.send('evm_mine'); // this one will have 02:00 PM as its timestamp
   }
-
-}
+};
 
 const initVaults = async (
   baseSushiVault: BaseSushiVault,
@@ -181,63 +183,51 @@ const initVaults = async (
   _rageClearingHouse: string,
   _rageCollateralToken: string,
   _rageBaseToken: string,
-  _sushiParams: SushiParamsStruct
+  _sushiParams: SushiParamsStruct,
 ) => {
+  await (
+    await baseSushiVault.initialize(_owner, _rageClearingHouse, _rageCollateralToken, _rageBaseToken, _sushiParams)
+  ).wait();
 
-  await (await baseSushiVault.initialize(
-    _owner,
-    _rageClearingHouse,
-    _rageCollateralToken,
-    _rageBaseToken,
-    _sushiParams
-  )).wait();
-
-  await (await vaultTest.initialize(
-    _owner,
-    _rageClearingHouse,
-    _rageCollateralToken,
-    _rageBaseToken,
-    _sushiParams
-  )).wait();
-}
+  await (
+    await vaultTest.initialize(_owner, _rageClearingHouse, _rageCollateralToken, _rageBaseToken, _sushiParams)
+  ).wait();
+};
 
 before(async () => {
-
   await activateMainnetFork({
     blockNumber: arbConstants.BLOCK,
-    network: 'arbitrum-mainnet'
+    network: 'arbitrum-mainnet',
   });
-
-})
+});
 
 after(async () => {
   await deactivateMainnetFork();
-})
+});
 
 describe('# Main', () => {
+  let vBase: IVBase;
+  let vToken: IVToken;
+  let realBase: IERC20;
+  let realToken: IWETH;
+  let clearingHouse: ClearingHouse;
+  let oracle: IOracle;
+  let vPool: IUniswapV3Pool;
+  let vPoolWrapper: IVPoolWrapper;
 
-  let vBase: IVBase
-  let vToken: IVToken
-  let realBase: IERC20
-  let realToken: IWETH
-  let clearingHouse: ClearingHouse
-  let oracle: IOracle
-  let vPool: IUniswapV3Pool
-  let vPoolWrapper: IVPoolWrapper
+  let uniV2Factory: IUniswapV2Factory;
+  let uniV2Router: IUniswapV2Router02;
+  let sushiChef: IMiniChefV2;
+  let weth: IWETH;
+  let usdc: IERC20;
+  let wethUsdc: IUniswapV2Pair;
+  let sushiToken: IERC20;
 
-  let uniV2Factory: IUniswapV2Factory
-  let uniV2Router: IUniswapV2Router02
-  let sushiChef: IMiniChefV2
-  let weth: IWETH
-  let usdc: IERC20
-  let wethUsdc: IUniswapV2Pair
-  let sushiToken: IERC20
+  let usdcOracle: FakeContract<IAggregatorV3Interface>;
+  let wethOracle: FakeContract<IAggregatorV3Interface>;
 
-  let usdcOracle: FakeContract<IAggregatorV3Interface>
-  let wethOracle: FakeContract<IAggregatorV3Interface>
-
-  let baseSushiVault: BaseSushiVault
-  let vaultTest: VaultTest
+  let baseSushiVault: BaseSushiVault;
+  let vaultTest: VaultTest;
 
   let admin: SignerWithAddress;
   let alice: SignerWithAddress;
@@ -245,39 +235,19 @@ describe('# Main', () => {
   let carol: SignerWithAddress;
 
   describe('# Setup & Deployments', () => {
-
     it('- deploying and creating accounts...', async () => {
+      ({ vBase, vToken, realBase, realToken, clearingHouse, oracle, vPool, vPoolWrapper } = await deployAndSetupCore());
 
-      ({
-        vBase,
-        vToken,
-        realBase,
-        realToken,
-        clearingHouse,
-        oracle,
-        vPool,
-        vPoolWrapper
-      } = await deployAndSetupCore());
-
-      ({
-        uniV2Factory,
-        uniV2Router,
-        sushiChef,
-        weth,
-        usdc,
-        wethUsdc,
-        sushiToken
-      } = await deployAndSetupHelpers());
+      ({ uniV2Factory, uniV2Router, sushiChef, weth, usdc, wethUsdc, sushiToken } = await deployAndSetupHelpers());
 
       ({ baseSushiVault, vaultTest } = await deployAndSetupVault(vToken.address));
 
       ({ admin, alice, bob, carol } = await createUsers());
 
       ({ usdcOracle, wethOracle } = await deployAndSetupChainlinkOralces());
-    })
+    });
 
     it('- should deploy without reverts', async () => {
-
       assert(vBase.address !== '0x0000000000000000000000000000000000000000', 'vBase');
       assert(vToken.address !== '0x0000000000000000000000000000000000000000', 'vToken');
       assert(realBase.address !== '0x0000000000000000000000000000000000000000', 'realBase');
@@ -296,7 +266,7 @@ describe('# Main', () => {
       assert(sushiToken.address !== '0x0000000000000000000000000000000000000000', 'sushiToken');
 
       assert(baseSushiVault.address !== '0x0000000000000000000000000000000000000000', 'baseSushiVault');
-    })
+    });
 
     it('- shoud set params correclty', async () => {
       // increases cardinality for twap
@@ -341,32 +311,23 @@ describe('# Main', () => {
       expect(protocol.removeLimitOrderFee).eq(removeLimitOrderFee);
       expect(protocol.minimumOrderNotional).eq(minimumOrderNotional);
       expect(curPaused).to.be.false;
-    })
+    });
 
     it('- shoud have default vaules because of no accounting actions', async () => {
-
       expect(await wethUsdc.token0()).eq(weth.address);
       expect(await wethUsdc.token1()).eq(usdc.address);
 
-      await initVaults(
-        baseSushiVault,
-        vaultTest,
-        admin.address,
-        clearingHouse.address,
-        weth.address,
-        usdc.address,
-        {
-          sushiRouter: uniV2Router.address,
-          sushiPair: wethUsdc.address,
-          sushiChef: sushiChef.address,
-          token0: await wethUsdc.token0(),
-          token1: await wethUsdc.token1(),
-          rewardToken: sushiToken.address,
-          token0Oracle: wethOracle.address,
-          token1Oracle: usdcOracle.address,
-          maxOracleDelayTime: 10 ** 6,
-        }
-      )
+      await initVaults(baseSushiVault, vaultTest, admin.address, clearingHouse.address, weth.address, usdc.address, {
+        sushiRouter: uniV2Router.address,
+        sushiPair: wethUsdc.address,
+        sushiChef: sushiChef.address,
+        token0: await wethUsdc.token0(),
+        token1: await wethUsdc.token1(),
+        rewardToken: sushiToken.address,
+        token0Oracle: wethOracle.address,
+        token1Oracle: usdcOracle.address,
+        maxOracleDelayTime: 10 ** 6,
+      });
 
       const pairDecimals = BigNumber.from(10).pow(await wethUsdc.decimals());
       const zeroBg = BigNumber.from(0);
@@ -391,17 +352,11 @@ describe('# Main', () => {
 
       expect(await baseSushiVault.previewRedeem(pairDecimals.mul(2))).eq(pairDecimals.mul(2));
       expect(await baseSushiVault.previewWithdraw(pairDecimals.mul(2))).eq(pairDecimals.mul(2));
-    })
+    });
 
     it('- cannot init twice', async () => {
-      await expect(initVaults(
-        baseSushiVault,
-        vaultTest,
-        admin.address,
-        clearingHouse.address,
-        weth.address,
-        usdc.address,
-        {
+      await expect(
+        initVaults(baseSushiVault, vaultTest, admin.address, clearingHouse.address, weth.address, usdc.address, {
           sushiRouter: uniV2Router.address,
           sushiPair: wethUsdc.address,
           sushiChef: sushiChef.address,
@@ -411,32 +366,27 @@ describe('# Main', () => {
           token0Oracle: wethOracle.address,
           token1Oracle: usdcOracle.address,
           maxOracleDelayTime: 10 ** 6,
-        }
-      )).to.revertedWith('Initializable: contract is already initialized');
-    })
-
-  })
+        }),
+      ).to.revertedWith('Initializable: contract is already initialized');
+    });
+  });
 
   describe('# Allowances', () => {
-
     it('- should have correct router allowances', async () => {
-
       expect(await weth.allowance(baseSushiVault.address, uniV2Router.address)).gte(0);
-    })
+    });
 
     it('- should have correct miniChef allowances', async () => {
-
       expect(await usdc.allowance(baseSushiVault.address, sushiChef.address)).gte(0);
-    })
-  })
+    });
+  });
 
   describe('# Deposit', () => {
-
     it('- should increase bal on transfer', async () => {
       const balBefore = await wethUsdc.balanceOf(alice.address);
 
       await hre.network.provider.request({
-        method: "hardhat_impersonateAccount",
+        method: 'hardhat_impersonateAccount',
         params: [arbConstants.WETH_USDC_WHALE],
       });
 
@@ -447,11 +397,10 @@ describe('# Main', () => {
 
       const balAfter = await wethUsdc.balanceOf(alice.address);
       expect(balAfter.sub(balBefore)).eq(BigNumber.from('241502161737011795'));
-    })
+    });
 
     it('- should deposit and mint vault tokens', async () => {
-
-      let vault = baseSushiVault.connect(alice)
+      let vault = baseSushiVault.connect(alice);
       let token = wethUsdc.connect(alice);
       token.approve(vault.address, '200000000000000000');
 
@@ -462,16 +411,15 @@ describe('# Main', () => {
       let balAfter = await vault.balanceOf(alice.address);
 
       expect(balAfter.sub(balBefore)).eq(await vault.balanceOf(alice.address));
-    })
+    });
 
     it('- should have correct getters after deposit', async () => {
-
       const pairDecimals = BigNumber.from(10).pow(await wethUsdc.decimals());
 
       // 1:1 peg
-      expect(await baseSushiVault.assetsOf(alice.address)).eq(BigNumber.from(
-        await baseSushiVault.balanceOf(alice.address)
-      ));
+      expect(await baseSushiVault.assetsOf(alice.address)).eq(
+        BigNumber.from(await baseSushiVault.balanceOf(alice.address)),
+      );
 
       expect(await baseSushiVault.assetsPerShare()).eq(BigNumber.from('1000000000000000000'));
       expect(await baseSushiVault.totalAssets()).eq(BigNumber.from('200000000000000000'));
@@ -486,62 +434,50 @@ describe('# Main', () => {
       expect(await baseSushiVault.previewWithdraw(pairDecimals.mul(2))).eq(BigNumber.from('2000000000000000000'));
 
       expect(await baseSushiVault.getMarketValue(BigNumber.from('1000000000000000000'))).gt(BigNumber.from(0));
-    })
-
-  })
+    });
+  });
 
   describe('# Stake', () => {
-
     let previous = BigNumber.from(0);
 
     it('- should auto stake SLP on deposit', async () => {
-      const chef = sushiChef.connect(alice)
+      const chef = sushiChef.connect(alice);
 
-      const [amount, rewardDebt] = await chef.userInfo(0, baseSushiVault.address)
+      const [amount, rewardDebt] = await chef.userInfo(0, baseSushiVault.address);
       previous = rewardDebt;
 
-      expect(amount).eq(await baseSushiVault.totalAssets())
+      expect(amount).eq(await baseSushiVault.totalAssets());
       expect(rewardDebt).gt(0);
-    })
+    });
 
     it('- should increase rewards after few blocks', async () => {
-      await mineBlocks(1000)
+      await mineBlocks(1000);
 
-      const chef = sushiChef.connect(alice)
+      const chef = sushiChef.connect(alice);
 
-      const [amount, rewardDebt] = await chef.userInfo(0, baseSushiVault.address)
-      expect(amount).eq(await baseSushiVault.totalAssets())
+      const [amount, rewardDebt] = await chef.userInfo(0, baseSushiVault.address);
+      expect(amount).eq(await baseSushiVault.totalAssets());
       expect(rewardDebt).gte(previous);
-    })
-
-  })
+    });
+  });
 
   describe('# Withdraw', () => {
-
     it('- should withdraw previously deposited amount and burn shares', async () => {
       // TODO: uncomment after range  stratergy dependency is fixed
-  
       //   const vault = baseSushiVault.connect(alice)
-
       //   const resp = await vault.withdraw(
       //     BigNumber.from('100000000000000000'), // 50%
       //     bob.address,
       //     alice.address
       //   )
-
       //   console.log(resp)
-
-    })
+    });
 
     it('- should auto harvest SUSHI on withdraw', async () => {
       // TODO: uncomment after range  stratergy dependency is fixed
-
       //   const chef = sushiChef.connect(alice)
-
       //   const [_, rewardDebt] = await chef.userInfo(0, baseSushiVault.address)
       //   expect(rewardDebt).eq(0);
-    })
-
-  })
-
-})
+    });
+  });
+});
