b02209520f7ca5ab492dc8cfadf70e9411a39912
LeadDev <leaddev@anon.farm>
2022-03-21 15:51:57 +0530

    style: fix formatting

--- contracts/interfaces/IWETH.sol
+++ contracts/interfaces/IWETH.sol
@@ -14,7 +14,11 @@ interface IWETH {
 
     function approve(address spender, uint256 amount) external returns (bool);
 
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
 
     function decimals() external view returns (uint256);
 }
