902968038c9c629e9e2cfa8e85256c01eed4c99e
AnonFinDev <findev@rage.trade>
2022-03-26 01:33:39 +0530

    test: added rage check helpers

---
+++ test/utils/rageChecks.ts
@@ -0,0 +1,52 @@
+import { ClearingHouse } from '@ragetrade/core/typechain-types';
+import { LiquidityPositionViewStructOutput } from '@ragetrade/core/typechain-types/IClearingHouse';
+import { expect } from 'chai';
+import { BigNumber, BigNumberish } from 'ethers';
+
+export async function getLiquidityPosition(
+  clearingHouse: ClearingHouse,
+  accountNo: BigNumber,
+  poolSerialNo: number,
+  liquidityPositionSerialNo: number,
+): Promise<LiquidityPositionViewStructOutput> {
+  const accountInfo = await clearingHouse.getAccountInfo(accountNo);
+  return accountInfo.tokenPositions[poolSerialNo].liquidityPositions[liquidityPositionSerialNo];
+}
+
+export async function getNetTokenPosition(
+  clearingHouse: ClearingHouse,
+  accountNo: BigNumber,
+  poolId: BigNumberish,
+): Promise<BigNumber> {
+  return clearingHouse.getAccountNetTokenPosition(accountNo, poolId);
+}
+
+export async function checkLiquidityPosition(
+  clearingHouse: ClearingHouse,
+  accountNo: BigNumber,
+  poolSerialNo: number,
+  liquidityPositionSerialNo: number,
+  baseTickLower: number,
+  baseTickUpper: number,
+  baseLiquidity: BigNumber,
+) {
+  const liquidityPosition = await getLiquidityPosition(
+    clearingHouse,
+    accountNo,
+    poolSerialNo,
+    liquidityPositionSerialNo,
+  );
+  expect(liquidityPosition.tickLower).to.eq(baseTickLower);
+  expect(liquidityPosition.tickUpper).to.eq(baseTickUpper);
+  expect(liquidityPosition.liquidity).to.eq(baseLiquidity);
+}
+
+export async function checkNetTokenPosition(
+  clearingHouse: ClearingHouse,
+  accountNo: BigNumber,
+  poolId: BigNumberish,
+  expectedNetTokenPosition: BigNumber,
+) {
+  const netTokenPosition = await getNetTokenPosition(clearingHouse, accountNo, poolId);
+  expect(netTokenPosition).to.eq(expectedNetTokenPosition);
+}
