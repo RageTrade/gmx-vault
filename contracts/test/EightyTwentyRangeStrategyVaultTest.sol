9a513ae415001540c54d46be9faa4eb2511ff000
AnonFinDev <findev@rage.trade>
2022-03-26 01:32:49 +0530

    test: updated eighty twenty vault test files

--- contracts/test/EightyTwentyRangeStrategyVaultTest.sol
+++ contracts/test/EightyTwentyRangeStrategyVaultTest.sol
@@ -10,8 +10,18 @@ import { ERC20 } from '@rari-capital/solmate/src/tokens/ERC20.sol';
 import { BaseVault } from '../base/BaseVault.sol';
 import { EightyTwentyRangeStrategyVault } from '../rangeStrategy/EightyTwentyRangeStrategyVault.sol';
 
+import { FixedPoint128 } from '@uniswap/v3-core-0.8-support/contracts/libraries/FixedPoint128.sol';
+
+import { FullMath } from '@uniswap/v3-core-0.8-support/contracts/libraries/FullMath.sol';
+import { console } from 'hardhat/console.sol';
+
 contract EightyTwentyRangeStrategyVaultTest is EightyTwentyRangeStrategyVault {
+    using FullMath for uint256;
+
     /* solhint-disable no-empty-blocks */
+    uint256 public priceX128;
+    address public settlementTokenTreasury;
+
     constructor(
         ERC20 _asset,
         string memory _name,
@@ -20,18 +30,43 @@ contract EightyTwentyRangeStrategyVaultTest is EightyTwentyRangeStrategyVault {
     ) BaseVault(_asset, _name, _symbol, _ethPoolId) {}
 
     function _stake(uint256 amount) internal virtual override {}
+    function initialize(
+        address _owner,
+        address _rageClearingHouse,
+        address _rageCollateralToken,
+        address _rageBaseToken,
+        uint16 _closePositionSlippageSqrtToleranceBps,
+        uint16 _resetPositionThresholdBps,
+        uint256 _priceX128,
+        address _settlementTokenTreasury
+    ) external initializer {
+        __BaseVault_init(_owner, _rageClearingHouse, _rageCollateralToken, _rageBaseToken);
+        __EightyTwentyRangeStrategyVault_init(_closePositionSlippageSqrtToleranceBps, _resetPositionThresholdBps);
+        priceX128 = _priceX128;
+        settlementTokenTreasury = _settlementTokenTreasury;
+    }
+
+    function _stake() internal virtual override {}
 
     function _harvestFees() internal virtual override {}
 
-    function getPriceX128() public view virtual override returns (uint256 priceX128) {}
+    function getPriceX128() public view virtual override returns (uint256) {
+        return priceX128;
+    }
 
-    function getMarketValue(uint256 amount) public view virtual override returns (uint256 marketValue) {}
+    function getMarketValue(uint256 amount) public view virtual override returns (uint256 marketValue) {
+        marketValue = amount.mulDiv(priceX128, FixedPoint128.Q128);
+    }
 
     // To convert yield token into USDC to cover loss on rage trade
-    function _withdrawBase(uint256 amount) internal virtual override {}
+    function _withdrawBase(uint256 amount) internal virtual override {
+        rageBaseToken.transferFrom(settlementTokenTreasury, address(this), amount);
+    }
 
     // To deposit the USDC profit made from rage trade into yield protocol
-    function _depositBase(uint256 amount) internal virtual override {}
+    function _depositBase(uint256 amount) internal virtual override {
+        rageBaseToken.transfer(settlementTokenTreasury, amount);
+    }
 
     function _stakedAssetBalance() internal view virtual override returns (uint256) {}
 
@@ -46,5 +81,9 @@ contract EightyTwentyRangeStrategyVaultTest is EightyTwentyRangeStrategyVault {
         returns (IClearingHouseStructures.LiquidityChangeParams[2] memory liquidityChangeParamList)
     {
         return _getLiquidityChangeParamsOnRebalance(vaultMarketValue);
+    }
+
+    function setYieldTokenPriceX128(uint256 _priceX128) external {
+        priceX128 = _priceX128;
     }
 }
--- test/EightyTwentyRangeStrategyVault.spec.ts
+++ test/EightyTwentyRangeStrategyVault.spec.ts
@@ -1,30 +1,99 @@
 import hre, { waffle } from 'hardhat';
 import { truncate } from '@ragetrade/core/test/utils/vToken';
 import { RageTradeSetup } from './setup/ragetrade';
-import { EightyTwentyRangeStrategyVaultTest } from '../typechain-types';
+import {
+  EightyTwentyRangeStrategyVaultTest,
+  ClearingHouse,
+  ERC20PresetMinterPauserUpgradeable,
+  SettlementTokenMock,
+  OracleMock,
+} from '../typechain-types';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { BigNumber } from 'ethers';
+import { parseTokenAmount } from '@ragetrade/core/test/utils/stealFunds';
+import { priceToPriceX128 } from '@ragetrade/core/test/utils/price-tick';
+let collateralTokenOracle: OracleMock;
 
 const { deployContract } = waffle;
 
 describe('EightyTwentyRangeStrategyVault', () => {
   let snapshotId: string;
   let rageTrade = new RageTradeSetup();
+  let clearingHouse: ClearingHouse;
   let test: EightyTwentyRangeStrategyVaultTest;
+  let collateralToken: ERC20PresetMinterPauserUpgradeable;
+  let settlementToken: SettlementTokenMock;
+  let yieldToken: ERC20PresetMinterPauserUpgradeable;
+  let collateralTokenOracle: OracleMock;
+  let vaultRageAccountNo: BigNumber;
+
+  let signers: SignerWithAddress[];
+  let admin: SignerWithAddress;
+  let user0: SignerWithAddress;
+  let user1: SignerWithAddress;
+  let settlementTokenTreasury: SignerWithAddress;
 
   before(async () => {
     console.log(await hre.ethers.provider.getBlockNumber());
     // setup rage trade
-    await rageTrade.setup();
+    const initialPriceX128 = await priceToPriceX128(4000, 6, 18);
+    await rageTrade.setup(initialPriceX128);
 
     // deploy EightyTwentyRangeStrategyVault
     const {
-      settlementToken,
+      settlementToken: _settlementToken,
       pools: [pool0],
+      clearingHouse: _clearingHouse,
     } = await rageTrade.getContracts();
+    settlementToken = _settlementToken;
+    clearingHouse = _clearingHouse;
+
+    const tokenFactory = await hre.ethers.getContractFactory('ERC20PresetMinterPauserUpgradeable');
+    collateralToken = await tokenFactory.deploy();
+    await collateralToken.initialize('Collateral Token', 'CT');
+    yieldToken = await tokenFactory.deploy();
+    await yieldToken.initialize('Yield Token', 'YT');
+
     test = await (
       await hre.ethers.getContractFactory('EightyTwentyRangeStrategyVaultTest')
-    ).deploy(settlementToken.address, 'Hello', 'Hello', truncate(pool0.vToken.address));
+    ).deploy(yieldToken.address, 'Vault Token', 'VT', truncate(pool0.vToken.address));
 
+    signers = await hre.ethers.getSigners();
+
+    admin = signers[0];
+    user0 = signers[1];
+    user1 = signers[2];
+    settlementTokenTreasury = signers[3];
+
+    const closePositionToleranceBps = 500; //5%
+    const resetPositionThresholdBps = 2000; //20%
+    await test.initialize(
+      admin.address,
+      clearingHouse.address,
+      collateralToken.address,
+      settlementToken.address,
+      closePositionToleranceBps,
+      resetPositionThresholdBps,
+      1n << 128n,
+      settlementTokenTreasury.address,
+    );
+    await test.grantAllowances();
+    collateralToken.grantRole(await collateralToken.MINTER_ROLE(), test.address);
+    collateralTokenOracle = await (await hre.ethers.getContractFactory('OracleMock')).deploy();
+
+    await clearingHouse.updateCollateralSettings(collateralToken.address, {
+      oracle: collateralTokenOracle.address,
+      twapDuration: 300,
+      isAllowedForDeposit: true,
+    });
+
+    vaultRageAccountNo = await test.rageAccountNo();
+    await yieldToken.mint(user0.address, parseTokenAmount(10n ** 10n, 18));
+    await yieldToken.connect(user0).approve(test.address, parseTokenAmount(10n ** 10n, 18));
     // take snapshot
+    await settlementToken.mint(settlementTokenTreasury.address, parseTokenAmount(10n ** 20n, 18));
+    await settlementToken.connect(settlementTokenTreasury).approve(test.address, parseTokenAmount(10n ** 20n, 18));
+
     snapshotId = await hre.ethers.provider.send('evm_snapshot', []);
   });
   afterEach(async () => {
@@ -32,16 +101,34 @@ describe('EightyTwentyRangeStrategyVault', () => {
     snapshotId = await hre.ethers.provider.send('evm_snapshot', []);
   });
 
-  describe('#rebalance', () => {
-    it('should not update ticks when price within thresholds', async () => {
+  describe('#Deposit', () => {
+    it('First Deposit', async () => {
+      await test.connect(user0).deposit(parseTokenAmount(10n ** 3n, 18), user0.address);
+
+      // console.log((await clearingHouse.getAccountInfo(vaultRageAccountNo)).tokenPositions[0].liquidityPositions[0]);
+      // console.log(await test.baseLiquidity());
+      // console.log(await test.baseTickLower());
+      // console.log(await test.totalAssets());
+      // console.log(await test.totalSupply());
       // test.getLiquidityChangeParamsOnRebalance();
     });
-    it('should update ticks when price outside thresholds within range');
-    it('should update ticks when price outside range');
   });
 
-  describe('#deposit', () => {
-    it('should work when no existing range and no vault liquidity');
-    it('should work when existing range and vault liquidity');
+  describe.skip('#Withdraw', () => {
+    before(async () => {
+      await test.connect(user0).deposit(parseTokenAmount(10n ** 3n, 18), user0.address);
+    });
+    it('Full Withdraw - Zero Assets afterwards', async () => {
+      // console.log(await clearingHouse.getAccountNetProfit(vaultRageAccountNo));
+      // console.log((await clearingHouse.getAccountInfo(vaultRageAccountNo)).tokenPositions[0].liquidityPositions[0]);
+
+      await test.connect(user0).withdraw(parseTokenAmount(10n ** 2n, 18), user0.address, user0.address);
+
+      // console.log((await clearingHouse.getAccountInfo(vaultRageAccountNo)).tokenPositions[0].liquidityPositions[0]);
+      // console.log(await test.baseLiquidity());
+      // console.log(await test.baseTickLower());
+      // console.log(await test.totalAssets());
+      // console.log(await test.totalSupply());
+    });
   });
 });
