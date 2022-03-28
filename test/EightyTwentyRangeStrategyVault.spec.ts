752708aedb20030e913b39eeaf1b69e78c44761e
AnonFinDev <findev@rage.trade>
2022-03-28 19:44:57 +0530

    test: updated basic deposit and withdraw cases

--- test/EightyTwentyRangeStrategyVault.spec.ts
+++ test/EightyTwentyRangeStrategyVault.spec.ts
@@ -1,6 +1,11 @@
 import hre from 'hardhat';
 import { parseTokenAmount } from '@ragetrade/core/test/utils/stealFunds';
-import { ClearingHouse, EightyTwentyRangeStrategyVaultTest, SettlementTokenMock } from '../typechain-types';
+import {
+  ClearingHouse,
+  EightyTwentyRangeStrategyVaultTest,
+  SettlementTokenMock,
+  VPoolWrapper__factory,
+} from '../typechain-types';
 import { eightyTwentyRangeStrategyFixture } from './fixtures/eighty-twenty-range-strategy-vault';
 import { BigNumber, BigNumberish } from 'ethers';
 import { expect } from 'chai';
@@ -28,7 +33,7 @@ describe('EightyTwentyRangeStrategyVault', () => {
   });
 
   describe('#Deposit', () => {
-    it('should perform first deposit', async () => {
+    it('First Deposit', async () => {
       const [, user0] = await hre.ethers.getSigners();
       const {
         eightyTwentyRangeStrategyVaultTest,
@@ -42,14 +47,15 @@ describe('EightyTwentyRangeStrategyVault', () => {
       } = await eightyTwentyRangeStrategyFixture();
 
       await eightyTwentyRangeStrategyVaultTest.connect(user0).deposit(parseTokenAmount(10n ** 3n, 18), user0.address);
-      await checkLiquidityPositionNum(clearingHouse, vaultAccountNo, 0, 1);
-      await checkLiquidityPosition(clearingHouse, vaultAccountNo, 0, 0, -202300, -184440, 4392187773328n);
-      await checkVaultRangeParams(eightyTwentyRangeStrategyVaultTest, -202300, -184440, 4392187773328n);
-      await checkNetTokenPosition(clearingHouse, vaultAccountNo, ethPoolId, -1n);
       await checkTotalAssets(eightyTwentyRangeStrategyVaultTest, parseTokenAmount(10n ** 3n, 18));
       await checkTotalSupply(eightyTwentyRangeStrategyVaultTest, parseTokenAmount(10n ** 3n, 18));
+      await checkVaultRangeParams(eightyTwentyRangeStrategyVaultTest, -202300, -184440, 4392187773328n);
 
-      //Collateral Token Balance 1e-6 USD low due to round down in market value calculation
+      await checkLiquidityPositionNum(clearingHouse, vaultAccountNo, 0, 1);
+      await checkLiquidityPosition(clearingHouse, vaultAccountNo, 0, 0, -202300, -184440, 4392187773328n);
+      await checkNetTokenPosition(clearingHouse, vaultAccountNo, ethPoolId, -1n);
+
+      //Collateral Token Balance 1e-6 USD lesser due to round down in market value calculation
       await checkRealTokenBalances(
         clearingHouse,
         vaultAccountNo,
@@ -62,7 +68,7 @@ describe('EightyTwentyRangeStrategyVault', () => {
   });
 
   describe('#Withdraw', () => {
-    it('should perform full withdraw with zero assets afterwards', async () => {
+    it('Partial Withdraw', async () => {
       const [, user0] = await hre.ethers.getSigners();
       const {
         eightyTwentyRangeStrategyVaultTest,
@@ -78,51 +84,69 @@ describe('EightyTwentyRangeStrategyVault', () => {
 
       await eightyTwentyRangeStrategyVaultTest
         .connect(user0)
-        .withdraw(parseTokenAmount(10n ** 3n, 18), user0.address, user0.address);
+        .withdraw(parseTokenAmount(10n ** 2n, 18), user0.address, user0.address);
 
-      await checkLiquidityPositionNum(clearingHouse, vaultAccountNo, 0, 0);
-      await checkVaultRangeParams(eightyTwentyRangeStrategyVaultTest, -202300, -184440, 0);
+      //900 - 2e-6 due to rounding down of account market value
+      const initialTotalAssets = parseTokenAmount(1000n, 18).sub(parseTokenAmount(2n, 12));
+      const finalTotalAssets = initialTotalAssets.sub(parseTokenAmount(100n, 18));
+      const initialTotalSupply = parseTokenAmount(1000n, 18);
+      const finalTotalSupply = initialTotalSupply.mul(finalTotalAssets).div(initialTotalAssets);
+      const initialLiquidity = BigNumber.from(4392187773328n);
+      const finalLiquidity = initialLiquidity.mul(finalTotalAssets).div(initialTotalAssets).add(1);
+
+      await checkTotalAssets(eightyTwentyRangeStrategyVaultTest, finalTotalAssets);
+      await checkTotalSupply(eightyTwentyRangeStrategyVaultTest, finalTotalSupply);
+      await checkVaultRangeParams(eightyTwentyRangeStrategyVaultTest, -202300, -184440, finalLiquidity);
+
+      await checkLiquidityPositionNum(clearingHouse, vaultAccountNo, 0, 1);
+      await checkLiquidityPosition(clearingHouse, vaultAccountNo, 0, 0, -202300, -184440, finalLiquidity);
       await checkNetTokenPosition(clearingHouse, vaultAccountNo, ethPoolId, -1n);
-      await checkTotalAssets(eightyTwentyRangeStrategyVaultTest, parseTokenAmount(0, 18));
-      await checkTotalSupply(eightyTwentyRangeStrategyVaultTest, parseTokenAmount(0, 18));
+
+      await checkRealTokenBalances(
+        clearingHouse,
+        vaultAccountNo,
+        collateralToken.address,
+        settlementToken.address,
+        parseTokenAmount(900n, 18),
+        0n,
+      );
     });
-  });
-
-  describe.skip('#Scenario1', () => {
-    it('Test Scenario 1', async () => {
+    it('Full Withdraw', async () => {
       const [, user0] = await hre.ethers.getSigners();
-
       const {
         eightyTwentyRangeStrategyVaultTest,
         clearingHouse,
         vaultAccountNo,
         settlementTokenTreasury,
+        ethPool,
+        collateralToken,
         settlementToken,
         ethPoolId,
       } = await eightyTwentyRangeStrategyFixture();
-      await eightyTwentyRangeStrategyVaultTest.connect(user0).deposit(parseTokenAmount(10n ** 3n, 18), user0.address);
-      await checkLiquidityPositionNum(clearingHouse, vaultAccountNo, 0, 1);
 
-      await increaseBlockTimestamp(100);
+      await eightyTwentyRangeStrategyVaultTest.connect(user0).deposit(parseTokenAmount(10n ** 3n, 18), user0.address);
+
+      //Withdraw all shares
       await eightyTwentyRangeStrategyVaultTest
         .connect(user0)
-        .withdraw(parseTokenAmount(10n ** 3n, 18), user0.address, user0.address);
+        .redeem(parseTokenAmount(10n ** 3n, 18), user0.address, user0.address);
 
-      // await swapToken(clearingHouse,user0,user0,ethPoolId,parseTokenAmount(1,18),0,false,false);
+      await checkTotalAssets(eightyTwentyRangeStrategyVaultTest, parseTokenAmount(0n, 18));
+      await checkTotalSupply(eightyTwentyRangeStrategyVaultTest, parseTokenAmount(0n, 18));
+      await checkVaultRangeParams(eightyTwentyRangeStrategyVaultTest, -202300, -184440, 0n);
 
-      await eightyTwentyRangeStrategyVaultTest.rebalance();
-      //Set starting price of pool
-      //Set timestamp to 0 (set real price)
-      //user0 deposits
-      //Set timestamp (set real price)
-      //user1 swaps on rage trade
+      await checkLiquidityPositionNum(clearingHouse, vaultAccountNo, 0, 0);
+      await checkNetTokenPosition(clearingHouse, vaultAccountNo, ethPoolId, 0n);
 
-      // swapToken(clearingHouse,)
-      //user2 deposits
-      //Rebalance
-      //Checks -
-
-      //Net token position (inside and outside)
+      //Initial loss due to precision errors
+      await checkRealTokenBalances(
+        clearingHouse,
+        vaultAccountNo,
+        collateralToken.address,
+        settlementToken.address,
+        parseTokenAmount(2n, 12),
+        0n,
+      );
     });
   });
 });
