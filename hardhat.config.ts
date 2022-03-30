02c4928cd749d97ab096d8697b9b4edf69c8c072
protodev-rage <protodev@rage.trade>
2022-03-30 21:53:18 +0530

    fix: hardhat config & add arb addresses

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -86,27 +86,7 @@ export default {
             },
           },
         },
-      },
-      {
-        version: '0.7.6',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 800,
-          },
-          metadata: {
-            // do not include the metadata hash, since this is machine dependent
-            // and we want all generated code to be deterministic
-            // https://docs.soliditylang.org/en/v0.7.6/metadata.html
-            bytecodeHash: 'none',
-          },
-          outputSelection: {
-            '*': {
-              '*': ['storageLayout'],
-            },
-          },
-        },
-      },
+      }
     ],
   },
   typechain: {
