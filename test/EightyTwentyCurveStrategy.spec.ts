74ec90d4e98d9f93faba2f566f50e4bc0e7e8aeb
AnonFinDev <findev@rage.trade>
2022-04-04 16:42:04 +0530

    test: fixed issue with integration test and fixtures

--- test/EightyTwentyCurveStrategy.spec.ts
+++ test/EightyTwentyCurveStrategy.spec.ts
@@ -5,13 +5,18 @@ import hre, { ethers } from 'hardhat';
 import addresses from './fixtures/addresses';
 import { eightyTwentyCurveStrategyFixture } from './fixtures/eighty-twenty-curve-strategy';
 
+const within = (value: BigNumber, start: BigNumber, end: BigNumber): Boolean => {
+  if (value.gte(start) && value.lte(end)) return true;
+  return false;
+};
+
 describe('EightyTwentyCurveStrategy', () => {
   before(async () => {
     await eightyTwentyCurveStrategyFixture();
   });
 
   describe('No Price Movement', () => {
-    it.only('Deposit - should transfer lp tokens & mint shares', async () => {
+    it.skip('Deposit - should transfer lp tokens & mint shares', async () => {
       const [admin, user1, user2] = await hre.ethers.getSigners();
       const { gauge, lpToken, curveYieldStrategyTest: curveYieldStrategy } = await eightyTwentyCurveStrategyFixture();
 
@@ -176,8 +181,8 @@ describe('EightyTwentyCurveStrategy', () => {
         vaultLpBalBefore,
         user1SharesBalBefore,
         user2SharesBalBefore,
-        totalSharesMintedBefore,
         totalAssetvalueBefore,
+        totalSharesMintedBefore,
       ] = await Promise.all([
         lpToken.balanceOf(user1.address),
         lpToken.balanceOf(user2.address),
@@ -185,15 +190,17 @@ describe('EightyTwentyCurveStrategy', () => {
         lpToken.balanceOf(curveYieldStrategy.address),
         curveYieldStrategy.balanceOf(user1.address),
         curveYieldStrategy.balanceOf(user2.address),
-        curveYieldStrategy.totalSupply(),
         curveYieldStrategy.totalAssets(),
+        curveYieldStrategy.totalSupply(),
       ]);
 
-      await curveYieldStrategy.connect(user1).withdraw(amount1, user1.address, user1.address);
+      const pricePerShareBefore = totalAssetvalueBefore.div(totalSharesMintedBefore);
+
+      const toWithdraw1 = await curveYieldStrategy.convertToAssets(user1SharesBalBefore);
+      await curveYieldStrategy.connect(user1).withdraw(toWithdraw1, user1.address, user1.address);
+
       await hre.network.provider.send('evm_increaseTime', [1_000_000]);
       await hre.network.provider.send('evm_mine', []);
-
-      const pricePerShareBefore = (await curveYieldStrategy.totalAssets()).div(await curveYieldStrategy.totalSupply());
 
       const [
         user1LpBalAfterFirstWithdraw,
@@ -211,15 +218,17 @@ describe('EightyTwentyCurveStrategy', () => {
         lpToken.balanceOf(curveYieldStrategy.address),
         curveYieldStrategy.balanceOf(user1.address),
         curveYieldStrategy.balanceOf(user2.address),
-        curveYieldStrategy.totalSupply(),
         curveYieldStrategy.totalAssets(),
+        curveYieldStrategy.totalSupply(),
       ]);
 
-      const pricePerShareAfterFirstWithdraw = (await curveYieldStrategy.totalAssets()).div(
-        await curveYieldStrategy.totalSupply(),
+      const pricePerShareAfterFirstWithdraw = totalAssetvalueAfterFirstWithdraw.div(
+        totalSharesMintedAfterFirstWithdraw,
       );
 
-      await curveYieldStrategy.connect(user2).withdraw(amount2, user2.address, user2.address);
+      const toWithdraw2 = await curveYieldStrategy.convertToAssets(user2SharesBalAfterFirstWithdraw);
+      await curveYieldStrategy.connect(user2).withdraw(toWithdraw2, user2.address, user2.address);
+
       await hre.network.provider.send('evm_increaseTime', [1_000_000]);
       await hre.network.provider.send('evm_mine', []);
 
@@ -239,9 +248,11 @@ describe('EightyTwentyCurveStrategy', () => {
         lpToken.balanceOf(curveYieldStrategy.address),
         curveYieldStrategy.balanceOf(user1.address),
         curveYieldStrategy.balanceOf(user2.address),
-        curveYieldStrategy.totalSupply(),
         curveYieldStrategy.totalAssets(),
+        curveYieldStrategy.totalSupply(),
       ]);
+
+      const pricePerShareSecondWithdraw = totalAssetvalueAfterSecondWithdraw.div(totalSharesMintedAfterSecondWithdraw);
 
       expect(user1LpBalBefore).to.be.eq(0);
       expect(user2LpBalBefore).to.be.eq(0);
@@ -255,29 +266,58 @@ describe('EightyTwentyCurveStrategy', () => {
       expect(totalAssetvalueBefore).to.be.gte(amount1.add(amount2));
       expect(totalSharesMintedBefore).to.be.eq(user1SharesBalBefore.add(user2SharesBalBefore));
 
-      // expect(user1LpBalAfterFirstWithdraw).to.be.gt(pricePerShareBefore.mul(user1SharesBalBefore));
-      // expect(user2LpBalAfterFirstWithdraw).to.be.eq(0);
+      expect(user1LpBalAfterFirstWithdraw).to.be.eq(pricePerShareBefore.mul(user1SharesBalBefore));
+      expect(user2LpBalAfterFirstWithdraw).to.be.eq(0);
 
-      // expect(vaultLpBalAfterFirstWithdraw).to.be.eq(0);
-      // expect(gaugeLpBalAfterFirstWithdraw).to.be.lt(gaugeLpBalBefore);
+      expect(vaultLpBalAfterFirstWithdraw).to.be.eq(0);
+      expect(gaugeLpBalAfterFirstWithdraw).to.be.lt(gaugeLpBalBefore);
 
-      // expect(user1SharesBalAfterFirstWithdraw).to.be.eq(0);
-      // expect(user2SharesBalAfterFirstWithdraw).to.be.eq(user2SharesBalBefore);
+      const fraction1 = user1SharesBalBefore.mul(totalAssetvalueBefore).div(totalSharesMintedBefore);
+      const shareFraction1 = await curveYieldStrategy.convertToShares(fraction1);
 
-      // expect(totalAssetvalueAfterFirstWithdraw).to.be.lt(totalAssetvalueBefore);
-      // expect(totalSharesMintedAfterFirstWithdraw).to.be.lt(user2SharesBalAfterFirstWithdraw);
+      expect(within(user1SharesBalAfterFirstWithdraw, BigNumber.from(0), user1SharesBalBefore.sub(shareFraction1))).to
+        .be.true;
+      expect(user2SharesBalAfterFirstWithdraw).to.be.eq(user2SharesBalBefore);
 
-      // expect(user1LpBalAfterSecondWithdraw).to.be.eq(user1LpBalAfterFirstWithdraw);
-      // expect(user2LpBalAfterSecondWithdraw).to.be.eq(pricePerShareAfterFirstWithdraw.mul(user2SharesBalAfterFirstWithdraw));
+      expect(within(totalAssetvalueBefore.sub(totalAssetvalueAfterFirstWithdraw), BigNumber.from(0), shareFraction1)).to
+        .be.true;
 
-      // expect(gaugeLpBalAfterSecondWithdraw).to.be.lt(gaugeLpBalAfterFirstWithdraw);
-      // expect(vaultLpBalAfterSecondWithdraw).to.be.eq(0);
+      expect(
+        within(
+          totalSharesMintedAfterFirstWithdraw.sub(user1SharesBalBefore),
+          BigNumber.from(0),
+          user1SharesBalBefore.sub(shareFraction1),
+        ),
+      ).to.be.true;
 
-      // expect(user1SharesBalAfterSecondWithdraw).to.be.eq(user1SharesBalAfterFirstWithdraw);
-      // expect(user2SharesBalAfterSecondWithdraw).to.be.eq(0);
+      const fraction2 = toWithdraw2.mul(totalAssetvalueAfterFirstWithdraw).div(totalSharesMintedAfterFirstWithdraw);
+      const shareFraction2 = await curveYieldStrategy.convertToShares(fraction2);
 
-      // expect(totalAssetvalueAfterSecondWithdraw).to.be.eq(0);
-      // expect(totalSharesMintedAfterSecondWithdraw).to.be.eq(0);
+      expect(user1LpBalAfterSecondWithdraw).to.be.eq(user1LpBalAfterFirstWithdraw);
+      expect(user2LpBalAfterSecondWithdraw).to.be.eq(toWithdraw2);
+
+      expect(vaultLpBalAfterSecondWithdraw).to.be.eq(0);
+      expect(gaugeLpBalAfterSecondWithdraw).to.be.lt(gaugeLpBalAfterFirstWithdraw);
+
+      expect(user1SharesBalAfterSecondWithdraw).to.be.eq(user1SharesBalAfterFirstWithdraw);
+      expect(
+        within(
+          user2SharesBalAfterSecondWithdraw,
+          BigNumber.from(0),
+          shareFraction2.sub(user2SharesBalAfterFirstWithdraw),
+        ),
+      ).to.be.true;
+
+      expect(totalAssetvalueAfterSecondWithdraw).to.be.eq(
+        totalAssetvalueAfterFirstWithdraw.sub(user2LpBalAfterSecondWithdraw),
+      );
+      expect(
+        within(
+          totalSharesMintedAfterSecondWithdraw,
+          BigNumber.from(0),
+          shareFraction2.sub(user2SharesBalAfterFirstWithdraw),
+        ),
+      ).to.be.true;
     });
   });
   describe('Protocol Fee Withdrawal', () => {
