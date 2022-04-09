018bb22805697bd07bfad56b20dd30be4972eb38
AnonFinDev <findev@rage.trade>
2022-04-09 12:37:48 +0530

    test: updated tests and removed logs

--- test/utils/curve-helper.ts
+++ test/utils/curve-helper.ts
@@ -63,11 +63,11 @@ export const swapEth = async (
   ]);
   const ONE = BigNumber.from(10).pow(18);
 
-  console.log('BEFORE SWAP ETH : ');
-  console.log('LP TOKEN PRICE', initialLpTokenPrice, '$');
-  console.log('USDT RESERVE', initialUsdtReserve, ' USDT');
-  console.log('WBTC RESERVE', initialWbtcReserve, ' WBTC');
-  console.log('WETH RESERVE', initialWethReserve, ' WETH');
+  // console.log('BEFORE SWAP ETH : ');
+  // console.log('LP TOKEN PRICE', initialLpTokenPrice, '$');
+  // console.log('USDT RESERVE', initialUsdtReserve, ' USDT');
+  // console.log('WBTC RESERVE', initialWbtcReserve, ' WBTC');
+  // console.log('WETH RESERVE', initialWethReserve, ' WETH');
 
   await weth.connect(wethWhale).transfer(signer.address, ONE.mul(amount));
   await weth.connect(signer).approve(triCrypto.address, ONE.mul(amount));
@@ -81,11 +81,11 @@ export const swapEth = async (
     lpOracle,
   );
 
-  console.log('AFTER SWAP ETH : ');
-  console.log('LP TOKEN PRICE', finalLpTokenPrice, '$');
-  console.log('USDT RESERVE', finalUsdtReserve, ' USDT');
-  console.log('WBTC RESERVE', finalWbtcReserve, ' WBTC');
-  console.log('WETH RESERVE', finalWethReserve, ' WETH');
+  // console.log('AFTER SWAP ETH : ');
+  // console.log('LP TOKEN PRICE', finalLpTokenPrice, '$');
+  // console.log('USDT RESERVE', finalUsdtReserve, ' USDT');
+  // console.log('WBTC RESERVE', finalWbtcReserve, ' WBTC');
+  // console.log('WETH RESERVE', finalWethReserve, ' WETH');
 };
 
 export const swapUsdt = async (
@@ -111,11 +111,11 @@ export const swapUsdt = async (
   ]);
   const POW_SIX = BigNumber.from(10).pow(6);
 
-  console.log('BEFORE SWAP USDT : ');
-  console.log('LP TOKEN PRICE', initialLpTokenPrice, '$');
-  console.log('USDT RESERVE', initialUsdtReserve, ' USDT');
-  console.log('WBTC RESERVE', initialWbtcReserve, ' WBTC');
-  console.log('WETH RESERVE', initialWethReserve, ' WETH');
+  // console.log('BEFORE SWAP USDT : ');
+  // console.log('LP TOKEN PRICE', initialLpTokenPrice, '$');
+  // console.log('USDT RESERVE', initialUsdtReserve, ' USDT');
+  // console.log('WBTC RESERVE', initialWbtcReserve, ' WBTC');
+  // console.log('WETH RESERVE', initialWethReserve, ' WETH');
 
   await usdt.connect(usdtWhale).transfer(signer.address, POW_SIX.mul(amount));
   await usdt.connect(signer).approve(triCrypto.address, POW_SIX.mul(amount));
@@ -129,11 +129,11 @@ export const swapUsdt = async (
     lpOracle,
   );
 
-  console.log('AFTER SWAP USDT : ');
-  console.log('LP TOKEN PRICE', finalLpTokenPrice, '$');
-  console.log('USDT RESERVE', finalUsdtReserve, ' USDT');
-  console.log('WBTC RESERVE', finalWbtcReserve, ' WBTC');
-  console.log('WETH RESERVE', finalWethReserve, ' WETH');
+  // console.log('AFTER SWAP USDT : ');
+  // console.log('LP TOKEN PRICE', finalLpTokenPrice, '$');
+  // console.log('USDT RESERVE', finalUsdtReserve, ' USDT');
+  // console.log('WBTC RESERVE', finalWbtcReserve, ' WBTC');
+  // console.log('WETH RESERVE', finalWethReserve, ' WETH');
 };
 
 export const accrueFees = async (
@@ -148,19 +148,19 @@ export const accrueFees = async (
 ) => {
   const lpTokensBefore = await curveYieldStrategy.totalAssets();
 
-  console.log('BEFORE ACCRUING FEES : ');
-  console.log('CLAIMABLE REWARDS (100% in CRV): ', await gauge.claimable_reward(address, crv.address));
-  console.log('CRV TOKENS IN VAULT (FEES, 10% in CRV) : ', await crv.balanceOf(address));
-  console.log('LP TOKENS FROM CRV FEES (90% CRV swapped): ', lpTokensBefore);
-  console.log('PRICE PER SHARE : ', await curveYieldStrategy.previewMint(10n ** 18n));
+  // console.log('BEFORE ACCRUING FEES : ');
+  // console.log('CLAIMABLE REWARDS (100% in CRV): ', await gauge.claimable_reward(address, crv.address));
+  // console.log('CRV TOKENS IN VAULT (FEES, 10% in CRV) : ', await crv.balanceOf(address));
+  // console.log('LP TOKENS FROM CRV FEES (90% CRV swapped): ', lpTokensBefore);
+  // console.log('PRICE PER SHARE : ', await curveYieldStrategy.previewMint(10n ** 18n));
 
   await gauge.claimable_reward_write(address, addresses.CRV);
 
-  console.log('AFTER ACCRUING FEES : ');
+  // console.log('AFTER ACCRUING FEES : ');
 
   const claimable = await gauge.claimable_reward(address, crv.address);
-  console.log('CLAIMABLE REWARDS (100% in CRV): ', claimable);
-  console.log(claimable.mul(9).div(10));
+  // console.log('CLAIMABLE REWARDS (100% in CRV): ', claimable);
+  // console.log(claimable.mul(9).div(10));
 
   const usdtOut = await quoter.callStatic.quoteExactInput(
     '0x11cdb42b0eb46d95f990bedd4695a6e3fa034978000bb882af49447d8a07e3bd95bd0d56f35241523fbab10001f4fd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9',
@@ -168,7 +168,7 @@ export const accrueFees = async (
   );
   const lpTokenOut = await triCrypto.calc_token_amount([usdtOut, 0, 0], true);
 
-  console.log('CRV TOKENS IN VAULT (FEES, 10% in CRV) : ', claimable.div(10));
-  console.log('LP TOKENS FROM CRV FEES (90% CRV swapped): ', lpTokenOut);
-  console.log('PRICE PER SHARE : ', await curveYieldStrategy.previewMint(10n ** 18n));
+  // console.log('CRV TOKENS IN VAULT (FEES, 10% in CRV) : ', claimable.div(10));
+  // console.log('LP TOKENS FROM CRV FEES (90% CRV swapped): ', lpTokenOut);
+  // console.log('PRICE PER SHARE : ', await curveYieldStrategy.previewMint(10n ** 18n));
 };
