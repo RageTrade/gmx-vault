eaf34fdd30e008b8503f3dcbeddd29718351fd37
LeadDev <leaddev@anon.farm>
2022-04-20 13:11:50 +0530

    chore: update deploy scripts

--- deploy/usdt.ts
+++ deploy/usdt.ts
@@ -6,13 +6,22 @@ import { getNetworkInfo } from './network-info';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {
-    deployments: { save },
+    deployments: { save, deploy, get },
+    getNamedAccounts,
   } = hre;
+
+  const { deployer } = await getNamedAccounts();
 
   const { USDT_ADDRESS } = getNetworkInfo(hre.network.config.chainId);
 
   if (USDT_ADDRESS === undefined) {
-    throw new Error('Mock deployment not implemented yet');
+    // deploying mock
+    await deploy('USDT', {
+      contract: 'ERC20PresetMinterPauser',
+      from: deployer,
+      log: true,
+      args: ['USDT', 'USDT'],
+    });
   } else {
     await save('USDT', { abi: IERC20Metadata__factory.abi, address: USDT_ADDRESS });
   }
