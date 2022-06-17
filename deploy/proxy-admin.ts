c61af67409da894da59e201afb0c9d1496320602
LeadDev <leaddev@anon.farm>
2022-06-17 18:58:02 +0530

    chore: proxy admin deployment refactored

--- deploy/proxy-admin.ts
+++ deploy/proxy-admin.ts
@@ -11,17 +11,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const { deployer } = await getNamedAccounts();
 
-  let coreDeployment: ContractDeployment | undefined;
   try {
     const networkName = getNetworkNameFromChainId(hre.network.config.chainId ?? 31337);
-    coreDeployment = await getDeployment('core', networkName, 'ProxyAdmin');
-  } catch {}
-
-  if (coreDeployment) {
-    // if core deployment contains a proxy admin, use it
+    // this throws if core deployment does not contain a proxy admin
+    const coreDeployment = await getDeployment('core', networkName, 'ProxyAdmin');
     await save('ProxyAdmin', { abi: ProxyAdmin__factory.abi, address: coreDeployment.address });
-  } else {
+  } catch {
     // if doesn't contain a proxy admin then deploy a new one
+    console.log('No core deployment found');
     await deploy('ProxyAdmin', {
       contract: 'ProxyAdmin',
       from: deployer,
