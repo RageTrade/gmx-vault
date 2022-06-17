f51d28e450611fcae5b34ff162493e09029e2821
LeadDev <leaddev@anon.farm>
2022-06-17 15:04:29 +0530

    chore: fix interface for CurveOracle deployment

--- deploy/curve-oracle.ts
+++ deploy/curve-oracle.ts
@@ -1,7 +1,7 @@
 import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 
-import { IERC20Metadata__factory } from '../typechain-types';
+import { AggregatorV3Interface__factory } from '../typechain-types';
 import { getNetworkInfo } from './network-info';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
@@ -22,7 +22,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       log: true,
     });
   } else {
-    await save('CurveOracle', { abi: IERC20Metadata__factory.abi, address: CURVE_USD_ORACLE });
+    await save('CurveOracle', { abi: AggregatorV3Interface__factory.abi, address: CURVE_USD_ORACLE });
   }
 };
 
