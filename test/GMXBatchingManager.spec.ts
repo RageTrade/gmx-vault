925e99f02c7328508f1253ac7eba80bb9b5441dd
0xDosa <findev@rage.trade>
2022-07-23 19:51:56 +0530

    test: added fixture and batching manager spec

--- test/GMXBatchingManager.spec.ts
+++ test/GMXBatchingManager.spec.ts
@@ -1,23 +1,107 @@
+import { FakeContract } from '@defi-wonderland/smock';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { parseTokenAmount, slippageToSqrtPriceLimit } from '@ragetrade/sdk';
 import { expect } from 'chai';
 import { BigNumber } from 'ethers';
 import hre, { ethers } from 'hardhat';
-import { GMXBatchingManager } from '../typechain-types';
+import { ERC20, GMXBatchingManager, GmxVaultMock, GMXYieldStrategy } from '../typechain-types';
 import addresses from './fixtures/addresses';
-import { gmxBatchingManagerFixture } from './fixtures/gmx-batcing-manager';
+import { gmxBatchingManagerFixture } from './fixtures/gmx-batching-manager';
 import { unlockWhales } from './utils/curve-helper';
+import { increaseBlockTimestamp } from './utils/vault-helpers';
 
-describe('Vault Periphery', () => {
+describe('GMX Batching Manager', () => {
   let admin: SignerWithAddress;
-  let vault: SignerWithAddress;
+  let vault: GmxVaultMock;
   let keeper: SignerWithAddress;
   let user1: SignerWithAddress;
   let user2: SignerWithAddress;
+  let usdc: ERC20;
+  let fsGlp: ERC20;
+  let sGlp: ERC20;
   let gmxBatchingManager: GMXBatchingManager;
   before(async () => {
     await gmxBatchingManagerFixture();
   });
   beforeEach(async () => {
-    ({ admin, vault, user1, gmxBatchingManager } = await gmxBatchingManagerFixture());
+    ({ admin, vault, user1,user2,keeper, usdc,fsGlp,sGlp, gmxBatchingManager } = await gmxBatchingManagerFixture());
   });
+  describe("Start State", () => {
+    it("initialized state", async() =>{
+        expect(await gmxBatchingManager.currentRound()).to.eq(1);
+        expect(await gmxBatchingManager.keeper()).to.eq(keeper.address);
+        expect(await gmxBatchingManager.gmxVault()).to.eq(vault.address);
+    })
+  })
+  describe("deposit", () => {
+    it("Single Deposit", async() => {
+        const depositAmount = parseTokenAmount(100n,6);
+
+        await expect(() => gmxBatchingManager.connect(user1).depositToken(usdc.address,depositAmount,user1.address))
+        .to.changeTokenBalance(usdc,user1,depositAmount.mul(-1n));
+
+        const user1Deposit = await gmxBatchingManager.userDeposits(user1.address);
+        // console.log(user1Deposit);
+        expect(user1Deposit.round).to.eq(1);
+        expect(user1Deposit.glpBalance).to.eq(await fsGlp.balanceOf(gmxBatchingManager.address));
+        expect(user1Deposit.unclaimedShares).to.eq(0);
+
+        expect(await gmxBatchingManager.roundGlpBalance()).to.eq(user1Deposit.glpBalance);
+        expect(await fsGlp.balanceOf(gmxBatchingManager.address)).to.eq(user1Deposit.glpBalance);
+
+    });
+  })
+
+  describe("Execute Batch Deposit", () => {
+    it("Single User Batch Deposit", async() => {
+
+        const depositAmount = parseTokenAmount(100n,6);
+
+        await gmxBatchingManager.connect(user1).depositToken(usdc.address,depositAmount,user1.address);
+
+        await increaseBlockTimestamp(15*60); //15 mins
+
+        const roundGlpBalance = await gmxBatchingManager.roundGlpBalance();
+        //Check sGlp transfer and vault share transfer
+        await expect(()=>gmxBatchingManager.connect(keeper).executeBatchDeposit())
+        .to.changeTokenBalances(fsGlp,[gmxBatchingManager,vault],[roundGlpBalance.mul(-1),roundGlpBalance])
+
+        expect(await vault.balanceOf(gmxBatchingManager.address)).to.eq(roundGlpBalance);
+
+        const user1Deposit = await gmxBatchingManager.userDeposits(user1.address);
+        const round1Deposit = await gmxBatchingManager.roundDeposits(1);
+
+        expect(user1Deposit.round).to.eq(1);
+        expect(user1Deposit.unclaimedShares).to.eq(0);
+        expect(await gmxBatchingManager.currentRound()).to.eq(2);
+        expect(await gmxBatchingManager.roundGlpBalance()).to.eq(0);
+
+        expect(round1Deposit.totalGlp).to.eq(round1Deposit.totalGlp);
+        expect(round1Deposit.totalShares).to.eq(round1Deposit.totalShares);
+
+    })
+  })
+
+  describe("Claim", () => {
+    it("Single User Claim", async() => {
+        const depositAmount = parseTokenAmount(100n,6);
+
+        await gmxBatchingManager.connect(user1).depositToken(usdc.address,depositAmount,user1.address);
+
+        await increaseBlockTimestamp(15*60); //15 mins
+
+        await gmxBatchingManager.connect(keeper).executeBatchDeposit();
+
+        const roundDeposit = await gmxBatchingManager.roundDeposits(1);
+
+        await expect(() =>gmxBatchingManager.connect(user1).claim(user1.address,roundDeposit.totalShares))
+        .to.changeTokenBalances(vault,[gmxBatchingManager,user1],[roundDeposit.totalShares.mul(-1),roundDeposit.totalShares]);
+        
+        const user1Deposit = await gmxBatchingManager.userDeposits(user1.address);
+
+        expect(user1Deposit.round).to.eq(1);
+        expect(user1Deposit.glpBalance).to.eq(0);
+        expect(user1Deposit.unclaimedShares).to.eq(0);
+    })
+  })
 });
---
+++ test/fixtures/gmx-batching-manager.ts
@@ -0,0 +1,66 @@
+import { GMX_ECOSYSTEM_ADDRESSES as gmxAddresses } from './addresses';
+import addresses from './addresses';
+import { deployments } from 'hardhat';
+import { curveYieldStrategyFixture } from './curve-yield-strategy';
+import { ERC20, GMXBatchingManager__factory, GMXYieldStrategy } from '../../typechain-types';
+import { ethers } from 'ethers';
+import { unlockWhales } from '../utils/curve-helper';
+import { smock } from '@defi-wonderland/smock';
+import { parseUnits } from 'ethers/lib/utils';
+import { parseTokenAmount } from '@ragetrade/sdk';
+
+export const gmxBatchingManagerFixture = deployments.createFixture(async hre => {
+  const [admin, keeper, user1, user2] = await hre.ethers.getSigners();
+  
+  await hre.network.provider.request({
+    method: 'hardhat_impersonateAccount',
+    params: [gmxAddresses.USDC_WHALE],
+  });
+
+  const usdcWhale = await hre.ethers.getSigner(gmxAddresses.USDC_WHALE);
+
+  const vault = await (await hre.ethers.getContractFactory('GmxVaultMock')).deploy();
+
+  const gmxBatchingManagerFactory = await hre.ethers.getContractFactory('GMXBatchingManager');
+
+  const gmxBatchingManager = await gmxBatchingManagerFactory.deploy();
+
+  await gmxBatchingManager.initialize(gmxAddresses.StakedGlp, gmxAddresses.RewardRouter, gmxAddresses.GlpManager, vault.address, keeper.address);
+  await gmxBatchingManager.grantAllowances();
+
+  const usdc = (await hre.ethers.getContractAt(
+    '@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20',
+    addresses.USDC,
+  )) as ERC20;
+
+  const fsGlp = (await hre.ethers.getContractAt(
+    '@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20',
+    gmxAddresses.FeeStakedGlp,
+  )) as ERC20;
+
+  const sGlp = (await hre.ethers.getContractAt(
+    '@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20',
+    gmxAddresses.StakedGlp,
+  )) as ERC20;
+
+
+  await usdc.connect(usdcWhale).transfer(user1.address,parseTokenAmount(1000,6))
+  await usdc.connect(usdcWhale).transfer(user2.address,parseTokenAmount(1000,6))
+
+  await usdc.connect(user1).approve(gmxBatchingManager.address,2n**255n)
+  await usdc.connect(user2).approve(gmxBatchingManager.address,2n**255n)
+
+
+  return {
+    admin,
+    usdc,
+    sGlp,
+    fsGlp,
+    vault,
+    keeper,
+    usdcWhale,
+    user1,
+    user2,
+    gmxBatchingManager,
+  };
+});
