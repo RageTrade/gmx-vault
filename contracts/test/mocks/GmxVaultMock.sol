738263c2524d211e30c2f4826ad5152b2db0d6ec
0xDosa <findev@rage.trade>
2022-07-23 19:55:43 +0530

    ref: fixed formatting issues

--- contracts/test/mocks/GmxVaultMock.sol
+++ contracts/test/mocks/GmxVaultMock.sol
@@ -7,12 +7,12 @@ import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 import { ERC20PresetMinterPauser } from '@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol';
 
-contract GmxVaultMock is ERC20PresetMinterPauser{
-
+contract GmxVaultMock is ERC20PresetMinterPauser {
     constructor() ERC20PresetMinterPauser('GMXShares', 'GMXShares') {}
-    function deposit(uint256 amount, address receiver) external returns(uint256) {
+
+    function deposit(uint256 amount, address receiver) external returns (uint256) {
         IERC20(0x2F546AD4eDD93B956C8999Be404cdCAFde3E89AE).transferFrom(msg.sender, address(this), amount);
-        _mint(receiver,amount);
+        _mint(receiver, amount);
         return amount;
     }
 }
