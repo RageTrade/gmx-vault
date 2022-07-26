4a61c0a99d5196bdc4310de1527fd587042a5d54
LeadDev <leaddev@anon.farm>
2022-07-26 20:43:52 +0530

    chore: add basic deployment of gys

---
+++ deploy/gmx-batching-manager.ts
@@ -0,0 +1,24 @@
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { waitConfirmations } from './network-info';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {
+    deployments: { deploy },
+    getNamedAccounts,
+  } = hre;
+
+  const { deployer } = await getNamedAccounts();
+
+  await deploy('GMXBatchingManager', {
+    contract: 'GMXBatchingManager',
+    from: deployer,
+    log: true,
+    waitConfirmations,
+  });
+};
+
+export default func;
+
+func.tags = ['GMXBatchingManager'];
+func.dependencies = ['GMXYieldStrategy'];
