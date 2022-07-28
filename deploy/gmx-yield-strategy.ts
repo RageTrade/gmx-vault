60085403c684a3c72bcb4b9551fa5967806fef2c
LeadDev <leaddev@anon.farm>
2022-07-28 10:58:52 +0530

    chore: update deploy scripts

--- deploy/gmx-yield-strategy.ts
+++ deploy/gmx-yield-strategy.ts
@@ -18,43 +18,43 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const networkInfo = getNetworkInfo(hre.network.config.chainId ?? 31337);
 
-  // const clearingHouseAddress: string = (await get('ClearingHouse')).address;
-  // const settlementTokenAddress: string = (await get('SettlementToken')).address;
-  // const ethPoolId: string = truncate((await get('ETH-vToken')).address);
+  const clearingHouseAddress: string = (await get('ClearingHouse')).address;
+  const settlementTokenAddress: string = (await get('SettlementToken')).address;
+  const ethPoolId: string = truncate((await get('ETH-vToken')).address);
 
-  // const collateralTokenDeployment = await get('CollateralToken');
+  const collateralTokenDeployment = await get('CollateralToken');
 
-  // const clearingHouseLens = ClearingHouseLens__factory.connect(
-  //   (await get('ClearingHouseLens')).address,
-  //   await hre.ethers.getSigner(deployer),
-  // );
+  const clearingHouseLens = ClearingHouseLens__factory.connect(
+    (await get('ClearingHouseLens')).address,
+    await hre.ethers.getSigner(deployer),
+  );
 
-  // const initializeArg: GMXYieldStrategy.GMXYieldStrategyInitParamsStruct = {
-  //   eightyTwentyRangeStrategyVaultInitParams: {
-  //     baseVaultInitParams: {
-  //       rageErc4626InitParams: {
-  //         asset: networkInfo.CURVE_TRICRYPTO_LP_TOKEN,
-  //         name: '80-20 TriCrypto Strategy',
-  //         symbol: 'TCS',
-  //       },
-  //       ethPoolId,
-  //       swapSimulator: (await get('SwapSimulator')).address,
-  //       clearingHouseLens: clearingHouseLens.address,
-  //       rageClearingHouse: clearingHouseAddress,
-  //       rageCollateralToken: collateralTokenDeployment.address,
-  //       rageSettlementToken: settlementTokenAddress,
-  //     },
-  //     closePositionSlippageSqrtToleranceBps: 150,
-  //     resetPositionThresholdBps: 2000,
-  //     minNotionalPositionToCloseThreshold: 100e6,
-  //   },
-  //   gmx: GMX_ECOSYSTEM_ADDRESSES.GMX,
-  //   glp: GMX_ECOSYSTEM_ADDRESSES.GLP,
-  //   weth: addresses.WETH,
-  //   esGMX: GMX_ECOSYSTEM_ADDRESSES.StakedGmx, // TODO needs to change
-  //   glpManager: GMX_ECOSYSTEM_ADDRESSES.GlpManager,
-  //   rewardRouter: GMX_ECOSYSTEM_ADDRESSES.RewardRouter,
-  // };
+  const initializeArg: GMXYieldStrategy.GMXYieldStrategyInitParamsStruct = {
+    eightyTwentyRangeStrategyVaultInitParams: {
+      baseVaultInitParams: {
+        rageErc4626InitParams: {
+          asset: networkInfo.SGLP_ADDRESS,
+          name: '80-20 TriCrypto Strategy',
+          symbol: 'TCS',
+        },
+        ethPoolId,
+        swapSimulator: (await get('SwapSimulator')).address,
+        clearingHouseLens: clearingHouseLens.address,
+        rageClearingHouse: clearingHouseAddress,
+        rageCollateralToken: collateralTokenDeployment.address,
+        rageSettlementToken: settlementTokenAddress,
+      },
+      closePositionSlippageSqrtToleranceBps: 150,
+      resetPositionThresholdBps: 2000,
+      minNotionalPositionToCloseThreshold: 100e6,
+    },
+    gmx: networkInfo.GMX_ADDRESS,
+    glp: networkInfo.GLP_ADDRESS,
+    weth: networkInfo.WETH_ADDRESS,
+    esGMX: networkInfo.ESGMX_ADDRESS, // TODO needs to change
+    glpManager: networkInfo.GLP_MANAGER_ADDRESS,
+    rewardRouter: networkInfo.REWARD_ROUTER_ADDRESS,
+  };
 
   const ProxyDeployment = await deploy('GMXYieldStrategy', {
     contract: 'TransparentUpgradeableProxy',
@@ -63,62 +63,77 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     args: [
       gmxYieldStrategyLogicDeployment.address,
       proxyAdminDeployment.address,
-      '0x',
-      // GMXYieldStrategy__factory.createInterface().encodeFunctionData('initialize', [initializeArg]),
+      GMXYieldStrategy__factory.createInterface().encodeFunctionData('initialize', [initializeArg]),
     ],
     estimateGasExtra: 1_000_000,
     waitConfirmations,
   });
   await save('GMXYieldStrategy', { ...ProxyDeployment, abi: gmxYieldStrategyLogicDeployment.abi });
 
-  // if (ProxyDeployment.newlyDeployed) {
-  //   await execute(
-  //     'CurveYieldStrategy',
-  //     { from: deployer, estimateGasExtra: 1_000_000, waitConfirmations },
-  //     'grantAllowances',
-  //   );
+  if (ProxyDeployment.newlyDeployed) {
+    await execute(
+      'GMXYieldStrategy',
+      { from: deployer, estimateGasExtra: 1_000_000, waitConfirmations },
+      'grantAllowances',
+    );
 
-  //   await execute(
-  //     'CurveYieldStrategy',
-  //     { from: deployer, estimateGasExtra: 1_000_000, waitConfirmations },
-  //     'updateBaseParams',
-  //     parseUnits(networkInfo.DEPOSIT_CAP_C3CLT.toString(), 18),
-  //     networkInfo.KEEPER_ADDRESS,
-  //     86400, // rebalanceTimeThreshold
-  //     500, // rebalancePriceThresholdBps
-  //   );
+    await execute(
+      'GMXYieldStrategy',
+      { from: deployer, estimateGasExtra: 1_000_000, waitConfirmations },
+      'updateBaseParams',
+      parseUnits(networkInfo.DEPOSIT_CAP_C3CLT.toString(), 18),
+      networkInfo.KEEPER_ADDRESS,
+      86400, // rebalanceTimeThreshold
+      500, // rebalancePriceThresholdBps
+    );
 
-  //   await execute(
-  //     'CurveYieldStrategy',
-  //     { from: deployer, estimateGasExtra: 1_000_000, waitConfirmations },
-  //     'updateCurveParams',
-  //     1000, // feeBps
-  //     100, // stablecoinSlippage
-  //     parseUnits('2', 18), // crvHarvestThreshold
-  //     500, // crvSlippageTolerance
-  //     networkInfo.CURVE_USD_ORACLE,
-  //   );
+    const gbm = await get('GMXBatchingManager');
+    await execute(
+      'GMXBatchingManager',
+      { from: deployer, estimateGasExtra: 1_000_000, waitConfirmations },
+      'initialize',
+      networkInfo.SGLP_ADDRESS,
+      networkInfo.REWARD_ROUTER_ADDRESS,
+      networkInfo.GLP_MANAGER_ADDRESS,
+      gbm.address,
+      networkInfo.KEEPER_ADDRESS,
+    );
 
-  //   const MINTER_ROLE = await read('CollateralToken', 'MINTER_ROLE');
-  //   await execute(
-  //     'CollateralToken',
-  //     { from: deployer, estimateGasExtra: 1_000_000, waitConfirmations },
-  //     'grantRole',
-  //     MINTER_ROLE,
-  //     ProxyDeployment.address,
-  //   );
+    await execute(
+      'GMXYieldStrategy',
+      { from: deployer, estimateGasExtra: 1_000_000, waitConfirmations },
+      'updateGMXParams',
+      1000, // feeBps
+      gbm.address, // batchingManager
+    );
 
-  //   // transfer ownership to team multisig
-  //   await execute(
-  //     'CurveYieldStrategy',
-  //     { from: deployer, estimateGasExtra: 1_000_000, waitConfirmations },
-  //     'transferOwnership',
-  //     networkInfo.MULTISIG,
-  //   );
-  // }
+    // transfer ownership to team multisig
+    await execute(
+      'GMXYieldStrategy',
+      { from: deployer, estimateGasExtra: 1_000_000, waitConfirmations },
+      'transferOwnership',
+      networkInfo.MULTISIG,
+    );
+
+    const MINTER_ROLE = await read('CollateralToken', 'MINTER_ROLE');
+    await execute(
+      'CollateralToken',
+      { from: deployer, estimateGasExtra: 1_000_000, waitConfirmations },
+      'grantRole',
+      MINTER_ROLE,
+      ProxyDeployment.address,
+    );
+  }
 };
 
 export default func;
 
 func.tags = ['GMXYieldStrategy'];
-func.dependencies = ['GMXYieldStrategyLogic', 'CollateralToken', 'ProxyAdmin', 'vETH', 'SwapSimulator'];
+func.dependencies = [
+  'GMXBatchingManager',
+  'GMXYieldStrategyLogic',
+  'CollateralToken',
+  'ProxyAdmin',
+  'vETH',
+  'SwapSimulator',
+];
