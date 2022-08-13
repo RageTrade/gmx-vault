086c51110587cd796890c11e67a9e35ee1758051
LeadDev <leaddev@anon.farm>
2022-08-13 11:31:58 +0530

    chore: update deploy scripts and mv to dir

---
+++ deploy/gmx-vault/glp-staking-manager-logic.ts
@@ -0,0 +1,23 @@
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { waitConfirmations } from '../network-info';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {
+    deployments: { deploy },
+    getNamedAccounts,
+  } = hre;
+
+  const { deployer } = await getNamedAccounts();
+
+  await deploy('GlpStakingManagerLogic', {
+    contract: 'GlpStakingManager',
+    from: deployer,
+    log: true,
+    waitConfirmations,
+  });
+};
+
+export default func;
+
+func.tags = ['GlpStakingManagerLogic'];
---
+++ deploy/gmx-vault/glp-staking-manager.ts
@@ -0,0 +1,48 @@
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { GlpStakingManager, GlpStakingManager__factory } from '../../typechain-types';
+import { getNetworkInfo, waitConfirmations } from '../network-info';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const {
+    deployments: { get, deploy, save, execute, read },
+    getNamedAccounts,
+  } = hre;
+
+  const { deployer } = await getNamedAccounts();
+
+  const proxyAdminDeployment = await get('ProxyAdmin');
+  const glpStakingManagerLogicDeployment = await get('GlpStakingManagerLogic');
+
+  const networkInfo = getNetworkInfo(hre.network.config.chainId ?? 31337);
+
+  const initializeArg: GlpStakingManager.GlpStakingManagerInitParamsStruct = {
+    rageErc4626InitParams: {
+      asset: networkInfo.SGLP_ADDRESS,
+      name: 'GlpStakingManager',
+      symbol: 'GSM',
+    },
+    weth: networkInfo.WETH_ADDRESS,
+    usdc: networkInfo.RAGE_SETTLEMENT_TOKEN_ADDRESS ?? (await get('SettlementToken')).address,
+    rewardRouter: networkInfo.REWARD_ROUTER_ADDRESS,
+  };
+
+  const proxyDeployment = await deploy('GlpStakingManager', {
+    contract: 'TransparentUpgradeableProxy',
+    from: deployer,
+    log: true,
+    args: [
+      glpStakingManagerLogicDeployment.address,
+      proxyAdminDeployment.address,
+      GlpStakingManager__factory.createInterface().encodeFunctionData('initialize', [initializeArg]),
+    ],
+    estimateGasExtra: 1_000_000,
+    waitConfirmations,
+  });
+  await save('GlpStakingManager', { ...proxyDeployment, abi: glpStakingManagerLogicDeployment.abi });
+};
+
+export default func;
+
+func.tags = ['GlpStakingManager'];
+func.dependencies = ['ProxyAdmin', 'GlpStakingManagerLogic', 'SettlementToken'];
