52fd6fbabd616a9f07d1c2bd1446f765b52ac6d5
LeadDev <leaddev@anon.farm>
2022-08-25 10:55:16 +0530

    feat: add IERC20 to ISGLP

--- contracts/interfaces/gmx/ISGLPExtended.sol
+++ contracts/interfaces/gmx/ISGLPExtended.sol
@@ -2,7 +2,9 @@
 
 pragma solidity ^0.8.0;
 
-interface ISGLPExtended {
+import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+interface ISGLPExtended is IERC20 {
     function glp() external view returns (address);
 
     function glpManager() external view returns (address);
