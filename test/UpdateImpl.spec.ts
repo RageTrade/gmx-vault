11c65415c89d2ffca4561adec174e2b4abcf7b3f
protodev-rage <protodev@rage.trade>
2022-09-03 22:20:14 +0530

    test: deposit fails if midway in migrating

--- test/UpdateImpl.spec.ts
+++ test/UpdateImpl.spec.ts
@@ -281,4 +281,141 @@ describe('Update Implementation', () => {
 
     await expect(tx3).to.emit(vaultWithLogicAbi, 'EightyTwentyParamsUpdated');
   });
+
+  it.only('tests updating implementation', async () => {
+    const {
+      deployments: { deploy },
+    } = hre;
+
+    const crv = (await hre.ethers.getContractAt(
+      '@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20',
+      addresses.CRV,
+    )) as ERC20;
+
+    const oldGauge = (await hre.ethers.getContractAt(
+      'contracts/interfaces/curve/ICurveGauge.sol:ICurveGauge',
+      addresses.OLD_GAUGE,
+    )) as ICurveGauge;
+
+    const newGauge = (await hre.ethers.getContractAt(
+      'contracts/interfaces/curve/ICurveGauge.sol:ICurveGauge',
+      addresses.NEW_GAUGE,
+    )) as ICurveGauge;
+
+    const gaugeFactory = (await hre.ethers.getContractAt(
+      'contracts/interfaces/curve/IGaugeFactory.sol:IGaugeFactory',
+      addresses.GAUGE_FACTORY,
+    )) as IGaugeFactory;
+
+    const lpToken = (await hre.ethers.getContractAt(
+      '@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20',
+      addresses.TRICRYPTO_LP_TOKEN,
+    )) as ERC20;
+
+    const lpOracle = (await hre.ethers.getContractAt(
+      'contracts/interfaces/curve/ILPPriceGetter.sol:ILPPriceGetter',
+      addresses.QUOTER,
+    )) as ILPPriceGetter;
+
+    const signers = await hre.ethers.getSigners();
+    const newUser = signers[0];
+
+    const owner = '0xee2a909e3382cdf45a0d391202aff3fb11956ad1';
+    const keeper = '0x0C0e6d63A7933e1C2dE16E1d5E61dB1cA802BF51';
+    const oldUser = '0x507c7777837b85ede1e67f5a4554ddd7e58b1f87';
+    const proxyAdmin = '0xA335Dd9CeFBa34449c0A89FB4d247f395C5e3782';
+    const triCryptoWhale = '0xAc27D1D01d1C2E29c8B567860c3f38123A4A9FEA';
+
+    const prevLogic = '0x96365da944537d027eCC9905f6b4237C093aE568';
+
+    const vaultWithLogicAbi = await hre.ethers.getContractAt(
+      'CurveYieldStrategy',
+      '0x1d42783E7eeacae12EbC315D1D2D0E3C6230a068',
+    );
+    const vaultWithProxyAbi = await hre.ethers.getContractAt(
+      'TransparentUpgradeableProxy',
+      '0x1d42783E7eeacae12EbC315D1D2D0E3C6230a068',
+    );
+
+    await hre.network.provider.request({
+      method: 'hardhat_impersonateAccount',
+      params: [owner],
+    });
+
+    await hre.network.provider.request({
+      method: 'hardhat_impersonateAccount',
+      params: [keeper],
+    });
+
+    await hre.network.provider.request({
+      method: 'hardhat_impersonateAccount',
+      params: [oldUser],
+    });
+
+    await hre.network.provider.request({
+      method: 'hardhat_impersonateAccount',
+      params: [proxyAdmin],
+    });
+
+    await hre.network.provider.request({
+      method: 'hardhat_impersonateAccount',
+      params: [vaultWithLogicAbi.address],
+    });
+
+    await hre.network.provider.request({
+      method: 'hardhat_impersonateAccount',
+      params: [triCryptoWhale],
+    });
+
+    const swapManager = await (await hre.ethers.getContractFactory('SwapManager')).deploy();
+
+    const logic = await (
+      await hre.ethers.getContractFactory('Logic', {
+        libraries: {
+          ['contracts/libraries/SwapManager.sol:SwapManager']: swapManager.address,
+        },
+      })
+    ).deploy();
+
+    const vaultLogicDeployment = await deploy('CurveYieldStrategyLogic', {
+      contract: 'CurveYieldStrategy',
+      libraries: {
+        SwapManager: swapManager.address,
+        Logic: logic.address,
+      },
+      from: newUser.address,
+      log: true,
+      waitConfirmations: undefined,
+    });
+
+    const vaultLogic = await hre.ethers.getContractAt('CurveYieldStrategy', vaultLogicDeployment.address);
+
+    const ownerSigner = await hre.ethers.getSigner(owner);
+    const keeperSigner = await hre.ethers.getSigner(keeper);
+    const oldUserSigner = await hre.ethers.getSigner(oldUser);
+    const proxyAdminSigner = await hre.ethers.getSigner(proxyAdmin);
+    const triCryptoWhaleSigner = await hre.ethers.getSigner(triCryptoWhale);
+    const vaultSigner = await hre.ethers.getSigner(vaultWithLogicAbi.address);
+
+    await vaultWithProxyAbi.connect(proxyAdminSigner).upgradeTo(vaultLogic.address);
+    // await vaultWithLogicAbi.connect(ownerSigner).updateCurveParams(
+    //   1000, // feeBps
+    //   100, // stablecoinSlippage
+    //   parseUnits('2', 18), // crvHarvestThreshold
+    //   500, // crvSlippageTolerance
+    //   addresses.NEW_GAUGE, // gauge
+    //   '0xaebDA2c976cfd1eE1977Eac079B4382acb849325', // networkInfo.CURVE_USD_ORACLE,
+    // );
+
+    // await vaultWithLogicAbi.connect(ownerSigner).grantAllowances();
+    // await vaultWithLogicAbi.connect(ownerSigner).migrate();
+
+    // new user is able to deposit
+    await lpToken.connect(triCryptoWhaleSigner).transfer(newUser.address, parseEther('1'));
+    await lpToken.connect(newUser).approve(vaultWithLogicAbi.address, ethers.constants.MaxUint256);
+
+    const tx = vaultWithLogicAbi.connect(newUser).deposit(parseEther('1'), newUser.address);
+
+    expect(tx).to.be.reverted;
+  });
 });
